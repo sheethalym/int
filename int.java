210a6c487cb5b3cd80e3231d9d76bf49eee42c93
sheethalym <sheethal.xworkz@gmail.com>
2021-12-20 21:40:06 +0530

    adding int program

---
+++ int.java
@@ -0,0 +1,17 @@
+class Int{
+ public static void main(String[]args){
+  int price[]={999,599,199,299,399,499,699,799,99,1500};
+  System.out.println(price[0]);
+ System.out.println(price[1]);
+ System.out.println(price[2]);
+ System.out.println(price[3]);
+ System.out.println(price[4]);
+ System.out.println(price[5]);
+ System.out.println(price[6]);
+ System.out.println(price[7]);
+ System.out.println(price[8]);
+ System.out.println(price[9]);
+
+ }
+
+}
